44aec7c096cd666c97b9b35015d45b8de5c7a3ff
E-AleynaElmas <a.aleynaelmas@gmail.com>
2021-02-16 01:54:50 +0300

    Game basics added.

---
+++ Flappy Bird/Assets/Scripts/BirdController.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BirdController : MonoBehaviour
+{
+    [SerializeField]
+    Sprite[] birdSprite;
+
+    [SerializeField]
+    Text scoreText;
+
+    [SerializeField]
+    GameObject gameController;
+
+    SpriteRenderer spriteRenderer;
+    bool motionControl = true;
+    int birdCounter = 0;
+    float birdMotionSpeed;
+    Rigidbody2D physics;
+    
+    int scoreCounter;
+    bool gameOver = true;
+    GameController gameControl;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        physics = GetComponent<Rigidbody2D>();
+        gameControl = gameControl.GetComponent<GameController>();
+    }
+
+    void Update()
+    {
+        //Tıklandığında yukarı kuvvet uygulanır
+        if (Input.GetMouseButtonDown(0) && gameOver)
+        {
+            physics.velocity = new Vector2(0, 0); //Hızı önce sıfırlıyoruz
+            physics.AddForce(new Vector2(0, 200)); //Sonra kuvvet uygulyoruz
+        }
+
+        //yukarı çıkarken yukarı bakış
+        if(physics.velocity.y > 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 45);
+        }
+        //aşağı düşerken aşağı bakış
+        else if(physics.velocity.y < 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, -45);
+        }
+
+        //Kanat çırpmak için animasyon
+        Animation();
+    }
+
+    void Animation()
+    {
+        birdMotionSpeed += Time.deltaTime;
+        if (birdMotionSpeed > 0.2f)
+        {
+            birdMotionSpeed = 0;
+
+            if (motionControl)
+            {
+                spriteRenderer.sprite = birdSprite[birdCounter];
+                birdCounter++;
+                if (birdCounter == birdSprite.Length)
+                {
+                    birdCounter--;
+                    motionControl = false;
+                }
+            }
+            else
+            {
+                birdCounter--;
+                spriteRenderer.sprite = birdSprite[birdCounter];
+                if (birdCounter == 0)
+                {
+                    birdCounter++;
+                    motionControl = true;
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        string colTag = col.gameObject.tag;
+        if(colTag == "TagScore")
+        {
+            scoreCounter++;
+            scoreText.text = scoreCounter + "";
+        }
+        if(colTag == "TagBarrier")
+        {
+            gameOver = false;
+            gameControl.GameOver();
+        }
+    }
+}
---
+++ Flappy Bird/Assets/Scripts/GameController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField]
+    GameObject sky1;
+
+    [SerializeField]
+    GameObject sky2;
+
+    [SerializeField]
+    float backGroundSpeed;
+
+    [SerializeField]
+    GameObject barrier;
+
+    [SerializeField]
+    int barriercount;
+
+    Rigidbody2D physics1;
+    Rigidbody2D physics2;
+    float skyLength;
+    GameObject[] barriers;
+    Rigidbody2D[] barriersRigidbody;
+    float speed;
+    int counter = 0;
+
+    void Start()
+    {
+        physics1 = sky1.GetComponent<Rigidbody2D>();
+        physics2 = sky2.GetComponent<Rigidbody2D>();
+
+        physics1.velocity = new Vector2(-backGroundSpeed, 0);
+        physics2.velocity = new Vector2(-backGroundSpeed, 0);
+
+        skyLength = sky1.GetComponent<BoxCollider2D>().size.x;
+
+        barriers = new GameObject[barriercount];
+
+        for(int i = 0; i < barriercount; i++)
+        {
+            barriers[i] = Instantiate(barrier, new Vector2(-20, -20), Quaternion.identity);
+            barriersRigidbody[i] = barriers[i].GetComponent<Rigidbody2D>();
+            barriersRigidbody[i].velocity = new Vector2(-backGroundSpeed, 0);
+        }
+    }
+
+    void Update()
+    {
+        if(sky1.transform.position.x <= -skyLength)
+        {
+            sky1.transform.position += new Vector3(skyLength * 2, 0);
+        }
+        if (sky2.transform.position.x <= -skyLength)
+        {
+            sky2.transform.position += new Vector3(skyLength * 2, 0);
+        }
+        //--------------------
+        speed += Time.deltaTime;
+        if (speed > 1.2f)
+        {
+            speed = 0;
+            float verticalAxis = Random.Range(-0.5f, 1.10f);
+            barriers[counter].transform.position = new Vector3(3, verticalAxis);
+            counter++;
+            if (counter >= barriers.Length)
+            {
+                counter = 0;
+            }
+        }
+    }
+
+    public void GameOver()
+    {
+        for(int i = 0; i < barriers.Length; i++)
+        {
+            barriersRigidbody[i].velocity = Vector2.zero;
+            physics1.velocity = Vector2.zero;
+            physics2.velocity = Vector2.zero;
+        }
+    }
+}
